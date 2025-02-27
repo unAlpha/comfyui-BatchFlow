c63f06119c59027da34d79e161b4ce19567030b6
彭银中 <pengyinzhong@pengyinzhongdeMac-mini.local>
2025-02-27 15:02:47 +0800

    first commit

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from .AgTools import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+__version__ = "0.1.0"
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+# WEB_DIRECTORY = "./web"
+
